d4577c761962d53849da5f6a42dcbe6ecd17112d
Raí Ernandes <ernandes.rai@gmail.com>
2024-06-20 21:43:50 -0300

    Exercicio javascript2

---
+++ Javascript ll/JS/exercicio.js
@@ -0,0 +1,77 @@
+function resposta(){
+    /*let a = 10;
+    let c = 10;
+
+    let d = [10 + 15,'10' + 2,'10' * 2, '10' / 3, '10' % 3, 10 + true, 10 == '10', 10 === '10', 10 < 11,
+         10 > 12, 10 <= 10.1, 10 > 9.99, 10 != 'dez', 10 + true, 'dez' + true, 10 + false, 10 * false, true + true, 
+         a++, c--, 1 & 1, 1 & 0, 0 & 0, 0 / 1, 5 + 5 == 10, '5' + '5' == 10, '5' * 2 > 9, (10 + 10) * 2, 10 + 10 * 2 ]*/    
+    
+    for(var cont = 0;  cont <= 1; cont++){
+        /*let b = d[cont];
+        console.log(b);
+        document.getElementById("resultado").innerHTML = "Resultado: " + b;*/
+
+        let a = 10 + 15; 
+        let b = '10' + 2;
+        let c = '10' * 2;
+        let d = '10' / 3;
+        let e = '10' % 3;
+        let f = 10 + true;
+        let g = 10 == '10';
+        let h = 10 === '10';
+        let i = 10 < 11;
+        let j = 10 > 12;
+        let k = 10 <= 10.1;
+        let l = 10 > 9.99;
+        let m = 10 != 'dez';
+        let n = 10 + true;
+        let o = 'dez' + true;
+        let p = 10 + false;
+        let q = 10 * false;
+        let r = true + true;
+        let s =  10;
+        s = s++;
+        let t = 10;
+        t = t++
+        let u = 1 & 1;
+        let v = 1 & 0;
+        let w = 0 & 0;
+        let x = 0 / 1;
+        let y = 5 + 5 == 10;
+        let z = '5' + '5' == 10;
+        let aa = '5' * 2 > 9;
+        let bb = (10 + 10) * 2;
+        let cc = 10 + 10 * 2;
+
+        console.log(a+"\n"+ b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g+"\n"+h+"\n"+i+"\n"+j+"\n"+
+            k+"\n"+l+"\n"+m+"\n"+n+"\n"+o+"\n"+p+"\n"+q+"\n"+r+"\n"+s+"\n"+t+"\n"+u+"\n"+v+"\n"+w+"\n"+x+"\n"+y+"\n"+z+"\n"+
+            aa+"\n"+bb+"\n"+cc);
+    }
+   
+}
+
+
+  
+
+
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
